8397880de38a7a19a517d020c4fa01687d75aa22
Matthew Incardona <matthewincardona@matthews-mini.wireless.rit.edu>
2022-10-23 17:12:25 -0400

    Initla commit

---
+++ pagebuilder.js
@@ -0,0 +1,37 @@
+// TO DO:
+// - put json reader in a separate file
+
+import drinkList from "./templates/item-list.js";
+
+const xhr = new XMLHttpRequest();
+const url = "data/items.json";
+let output = "";
+
+/// Load data with XMLHttpRequest
+xhr.addEventListener("load", function () {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    console.log(this.response);
+    const Items = JSON.parse(xhr.response);
+
+    // This just sets the entire response into localStorage
+    localStorage.setItem("Item Objects", JSON.stringify(Items));
+
+    for (let item of Items) {
+      output += drinkList(item);
+    }
+    addElement(output);
+  }
+
+  function addElement(data_input) {
+    // create a new div element, append it to main
+    const newDiv = document.createElement("div");
+    newDiv.id = "display";
+    newDiv.insertAdjacentHTML("afterbegin", data_input);
+
+    const currentDiv = document.getElementById("item-list");
+    currentDiv.appendChild(newDiv);
+  }
+});
+
+xhr.open("GET", url, true);
+xhr.send();
---
+++ templates/customize-item.js
@@ -0,0 +1,10 @@
+export default function drinkList(items) {
+  return `
+    <div class="item">
+        <img src="${items.thumbnail}" alt="" class="item__img" />
+        <div class="item__inner">
+            <h4 class="item__name">${items.name}</h4>
+            <p class="item__price">$${items.price}</p>
+        </div>
+    </div>`;
+}
