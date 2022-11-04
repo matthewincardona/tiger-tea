8a81a70dab500b719562e12ba8b8fa880779bebd
Matthew Incardona <matthewincardona@pop-os.localdomain>
2022-11-04 07:15:43 -0400

    Removing old files

--- builder/template-mapper.js
+++ builder/template-mapper.js
@@ -1,13 +1,10 @@
-import itemList from "../templates/item-list.js";
+// import itemList from "../templates/item-list.js";
 import customizeItem from "../templates/customize-item.js";
 import customizeBtns from "../templates/customize-btns.js";
 
 // maps template name to a function
 export function mapTemplate(moduleName, item) {
   switch (moduleName) {
-    case "item-list":
-      return itemList(item);
-      break;
     case "customize-item":
       return customizeItem(item);
       break;
