e216728e16586a517c5183cbe19fb346fe59c4f4
matthewincardona <doublethink.inc@gmail.com>
2022-11-09 03:48:30 -0500

    Edit item test

--- js/edit-cart-item.js
+++ js/edit-cart-item.js
@@ -6,14 +6,15 @@ import { getFromStorage } from '../utilities/get-from-storage.js';
 let params = new URL(document.location).searchParams;
 let position = params.get('productId');
 console.log('position: ' + position);
-var item;
+var item = {};
 
 const startBuild = () => {
   getFromStorage(position)
     .then((data) => {
+      item = data;
       console.log(data);
       buildModule('item-full', data);
-      setValue(data);
+      // setValue(data);
     })
     .catch((data) => {
       console.log(data);
@@ -22,27 +23,27 @@ const startBuild = () => {
 
 startBuild();
 
-function setValue(data) {
-  // set checked radio buttons
-  item = data;
-  console.log(item);
-  setSelectedValue('jelly', item.jelly);
-  setSelectedValue('tapioca-pearl', item.tapiocaPearl);
-  setSelectedValue('sweetness', item.sweetness);
-  setSelectedValue('ice', item.ice);
-}
+// function setValue(data) {
+//   // set checked radio buttons
+//   item = data;
+//   console.log(item);
+//   setSelectedValue('jelly', item.jelly);
+//   setSelectedValue('tapioca-pearl', item.tapiocaPearl);
+//   setSelectedValue('sweetness', item.sweetness);
+//   setSelectedValue('ice', item.ice);
+// }
 
-function setSelectedValue(selectedValue, itemValue) {
-  console.log(itemValue);
-  var ele = document.getElementsByName(selectedValue);
-  let i;
-  for (i = 0; i < ele.length; i++) {
-    if ((ele[i].value = itemValue)) {
-      console.log(ele[i].value);
-      ele[i].checked = 'true';
-    }
-  }
-}
+// function setSelectedValue(selectedValue, itemValue) {
+//   console.log(itemValue);
+//   var ele = document.getElementsByName(selectedValue);
+//   let i;
+//   for (i = 0; i < ele.length; i++) {
+//     if ((ele[i].value = itemValue)) {
+//       console.log(ele[i].value);
+//       ele[i].checked = 'true';
+//     }
+//   }
+// }
 
 // create cart object
 document.getElementById('add-to-cart').addEventListener('click', () => {
